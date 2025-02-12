91c3d70ed18b314a29e9e1052eb12c953ae7d82c
darthwallace <wallaceleite915@gmail.com>
2025-02-12 19:19:58 -0300

    Organizando a selecao de conteudos

---
+++ Js/script.js
@@ -0,0 +1,101 @@
+const mostrar = document.getElementById('mostrar_mensagem');
+const estrutura = document.getElementById('estrutura');
+const atribuicao = document.getElementById('atribuicao');
+const operacoes = document.getElementById('operacoes');
+const interatividade = document.getElementById('interatividade');
+const contatenacao = document.getElementById('contatenacao');
+const condicionais = document.getElementById('condicionais');
+const btn_7 = document.getElementById('btn_7');
+const dados = document.getElementById('dados');
+const variavel = document.getElementById('variavel');
+const sequencial = document.getElementById('sequencial');
+
+$(estrutura).mousemove(function(){
+    $(mostrar).css('display','block');
+    mostrar.innerHTML = "<p> Conteúdos sobre estrutura de dados. Let's Go </p>"
+});
+    
+$(estrutura).mouseout(function(){
+    $(mostrar).css('display','none');
+});
+
+$(atribuicao).mousemove(function(){
+    $(mostrar).css('display','block');
+    mostrar.innerHTML = "<p> Conteúdos sobre estrutura de dados. Let's Go </p>"
+});
+    
+$(atribuicao).mouseout(function(){
+    $(mostrar).css('display','none');
+});
+
+$(operacoes).mousemove(function(){
+    $(mostrar).css('display','block');
+    mostrar.innerHTML = "<p> Conteúdos sobre estrutura de dados. Let's Go </p>"
+});
+    
+$(operacoes).mouseout(function(){
+    $(mostrar).css('display','none');
+});
+
+$(interatividade).mousemove(function(){
+    $(mostrar).css('display','block');
+    mostrar.innerHTML = "<p> Conteúdos sobre estrutura de dados. Let's Go </p>"
+});
+    
+$(interatividade).mouseout(function(){
+    $(mostrar).css('display','none');
+});
+
+$(contatenacao).mousemove(function(){
+    $(mostrar).css('display','block');
+    mostrar.innerHTML = "<p> Conteúdos sobre estrutura de dados. Let's Go </p>"
+});
+    
+$(contatenacao).mouseout(function(){
+    $(mostrar).css('display','none');
+});
+
+$(condicionais).mousemove(function(){
+    $(mostrar).css('display','block');
+    mostrar.innerHTML = "<p> Conteúdos sobre estrutura de dados. Let's Go </p>"
+});
+    
+$(condicionais).mouseout(function(){
+    $(mostrar).css('display','none');
+});
+
+$(btn_7).mousemove(function(){
+    $(mostrar).css('display','block');
+    mostrar.innerHTML = "<p> Conteúdos sobre estrutura de dados. Let's Go </p>"
+});
+    
+$(btn_7).mouseout(function(){
+    $(mostrar).css('display','none');
+});
+
+$(dados).mousemove(function(){
+    $(mostrar).css('display','block');
+    mostrar.innerHTML = "<p> Conteúdos sobre estrutura de dados. Let's Go </p>"
+});
+    
+$(dados).mouseout(function(){
+    $(mostrar).css('display','none');
+});
+
+$(variavel).mousemove(function(){
+    $(mostrar).css('display','block');
+    mostrar.innerHTML = "<p> Conteúdos sobre estrutura de dados. Let's Go </p>"
+});
+    
+$(variavel).mouseout(function(){
+    $(mostrar).css('display','none');
+});
+
+$(sequencial).mousemove(function(){
+    $(mostrar).css('display','block');
+    mostrar.innerHTML = "<p> Conteúdos sobre estrutura de dados. Let's Go </p>"
+});
+    
+$(sequencial).mouseout(function(){
+    $(mostrar).css('display','none');
+});
